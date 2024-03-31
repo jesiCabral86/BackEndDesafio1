59792ff858a8185fee06aa94e78ba52b763c12f4
jesicabral <jesicabral@hotmail.com>
2024-03-31 01:32:53 -0300

    hacemos test

--- Desafio/productManager.js
+++ Desafio/productManager.js
@@ -2,9 +2,9 @@
 let products = [];
 
 //creamos metodo addProducts para agregar productos al arreglo inicial
-const addProducts = (title, description, price, thumbail, code, stock) => {
+const addProduct = (title, description, price, thumbail, code, stock) => {
 
-const newProducts = () => {
+const newProduct = {
     id: products.length + 1,
     title,
     description,
@@ -12,7 +12,7 @@ const newProducts = () => {
     thumbail,
     code,
     stock
-}
+};
 
 //validamos que no se repita el campo code
 const productExists = products.find (product => product.code === code);
@@ -21,7 +21,7 @@ if (productExists){
     return;
 }
 
-products.push (newProducts);
+products.push(newProduct);
 }
 
 const getProducts = () => {
@@ -29,19 +29,19 @@ const getProducts = () => {
     return products;
 }
 
-const getProductsById = (id) => {
-    const product = product.find (product => product.id === id);
-    if (!product) {
+const getProductById = (id) => {
+    const product = products.find (product => product.id === id);
+    if (! product) {
         console.log ("No se encontro el producto con el ID ${id}");
         return;
     }
 }
 
 //hacemos el test del ingreso de los productos
- addProducts("Producto 1", "El primer producto", 299, "http://www.google.com", "ASDF121", 10);
- addProducts("Producto 2", "El segundo producto", 899, "http://www.google.com", "ASDF122", 10);
- addProducts("Producto 3", "El tercer producto", 899, "http://www.google.com", "ASDF122", 10);
- addProducts("Producto 4", "El cuarto producto", 899, "http://www.google.com", "ASDF123", 10);
- addProducts("Producto 5", "El quinto producto", 899, "http://www.google.com", "ASDF124", 10);
+ addProduct("Producto 1", "El primer producto", 299, "http://www.google.com", "ASDF121", 10);
+ addProduct("Producto 2", "El segundo producto", 899, "http://www.google.com", "ASDF122", 10);
+ addProduct("Producto 3", "El tercer producto", 899, "http://www.google.com", "ASDF122", 10);
+ addProduct("Producto 4", "El cuarto producto", 899, "http://www.google.com", "ASDF123", 10);
+ addProduct("Producto 5", "El quinto producto", 899, "http://www.google.com", "ASDF124", 10);
 
  getProducts();
